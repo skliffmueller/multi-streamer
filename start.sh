421833e5ed9713f8c521c3149473d044a9dff55a
Shannon Kliffmueller <s.kliffmueller@gmail.com>
2023-03-21 11:32:08 -0500

    Lets encrypt

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 ln -sf /etc/nginx/nginx-letsencrypt.conf /etc/nginx/nginx.conf
 nginx
-# acme-nginx -d live.cppthink.org
+acme-nginx -d live.cppthink.org
 nginx -s stop
 sleep 5
 ln -sf /etc/nginx/nginx-default.conf /etc/nginx/nginx.conf
