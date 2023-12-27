53ee9a133b774b0d720b4d71c3eab6afa6301e23
Shannon Kliffmueller <s.kliffmueller@gmail.com>
2023-12-26 19:48:49 -0600

    Updated domain in script

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ if [ -z ${NO_SSL+x} ]
 then
   ln -sf /etc/nginx/nginx-letsencrypt.conf /etc/nginx/nginx.conf
   nginx
-  acme-nginx -d live.cppthink.org
+  acme-nginx -d streambox.rasterized.net
   nginx -s stop
   sleep 5
   ln -sf /etc/nginx/nginx-default.conf /etc/nginx/nginx.conf
